bd646479ab00375001966c961b625be3a702db80
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2020-01-29 19:57:38 +0200

    Add move logic on key down

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,42 @@ const INITIAL_BOARD_STATE: Board = [
   [undefined, undefined, undefined, undefined],
 ];
 
+const transposeBoard = board =>
+  board[0].map((col, i) => board.map(row => row[i]));
+
+const getNonEmptyCells = seq => seq.filter(cell => cell !== undefined);
+
+// TODO: pure func
+// TODO: types
+const sumCells = seq => {
+  seq.forEach((cell, index, array) => {
+    if (cell === undefined || array[index + 1] === undefined) return;
+
+    if (cell === array[index + 1]) {
+      array[index] = cell * 2;
+      array[index + 1] = undefined;
+    }
+  });
+
+  return seq;
+};
+
+const makeMoveOnSequence = seq => {
+  if (seq.every(cell => cell === undefined)) return seq;
+
+  const newSeq = getNonEmptyCells(sumCells(getNonEmptyCells(seq)));
+  const emptyCellsQuantity = seq.length - newSeq.length;
+
+  return newSeq.concat(...Array(emptyCellsQuantity));
+};
+
 const getInitialBoard = () =>
   generateBoardWithNewCell(generateBoardWithNewCell(INITIAL_BOARD_STATE));
 
-const INITIAL_BOARD = getInitialBoard();
+const FIRST_GENERATED_BOARD = getInitialBoard();
 
 const App: React.FC = () => {
-  const [board, setBoard] = useState(INITIAL_BOARD);
+  const [board, setBoard] = useState(FIRST_GENERATED_BOARD);
   const [isGameOver, setGameOver] = useState(false);
 
   const startNewGame = () => setBoard(getInitialBoard());
@@ -29,18 +58,48 @@ const App: React.FC = () => {
     const handleKeyDown = ({ key }: KeyboardEvent) => {
       switch (key) {
         case 'ArrowUp': {
+          setBoard(prevBoard => {
+            const transposedBoard = transposeBoard(prevBoard);
+            const boardAfterMove = transposedBoard.map(makeMoveOnSequence);
+
+            return transposeBoard(boardAfterMove) as Board;
+          });
+
           setBoard(prevBoard => generateBoardWithNewCell(prevBoard));
+
           break;
         }
         case 'ArrowDown': {
+          setBoard(prevBoard => {
+            const transposedBoard = transposeBoard(prevBoard);
+            const boardAfterMove = transposedBoard.map(col =>
+              makeMoveOnSequence(col.slice().reverse())
+                .slice()
+                .reverse(),
+            );
+
+            return transposeBoard(boardAfterMove) as Board;
+          });
+
           setBoard(prevBoard => generateBoardWithNewCell(prevBoard));
           break;
         }
         case 'ArrowLeft': {
+          setBoard(prevBoard => prevBoard.map(makeMoveOnSequence) as Board);
           setBoard(prevBoard => generateBoardWithNewCell(prevBoard));
           break;
         }
         case 'ArrowRight': {
+          setBoard(prevBoard => {
+            const boardAfterMove = prevBoard.map(row =>
+              makeMoveOnSequence(row.slice().reverse())
+                .slice()
+                .reverse(),
+            );
+
+            return boardAfterMove as Board;
+          });
+
           setBoard(prevBoard => generateBoardWithNewCell(prevBoard));
           break;
         }
